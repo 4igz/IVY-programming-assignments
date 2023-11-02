e11f726b60307a8146741e088aa3c2a7ea6c4f96
igz <78612868+4igz@users.noreply.github.com>
2023-11-02 09:06:51 -0400

    Update M02_Lab.py

--- M02_Lab.py
+++ M02_Lab.py
@@ -2,11 +2,16 @@
 # GPA tester
 # Will test gpas and tell user if a person has made Dean's list or Honor roll.
 while True:
-    last_name = input("What's your last name?: ")
+    last_name = input("What's your last name?: ").strip().capitalize()
     if last_name == "ZZZ":
         break # Not sure why we do this
-    first_name = input("What's your first name?: ")
-    gpa = float(input("What's your GPA?: "))
+    first_name = input("What's your first name?: ").strip().capitalize()
+    gpa = None
+    while not gpa:
+        try:
+            gpa = float(input("What's your GPA?: "))
+        except ValueError:
+            print("Value entered must be a number.")
     if gpa > 3.5:
         print(f"{first_name} {last_name} has made the Dean's list.")
     elif gpa > 3.25:
